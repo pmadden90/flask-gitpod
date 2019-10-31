011f96fc906d333a583252f4bfcaf755b2d5d735
Paul Madden <p.madden90@gmail.com>
2019-10-31 09:11:45 +0000

    Contact form working

--- flask/run.py
+++ flask/run.py
@@ -1,8 +1,9 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+app.secret_key = 'some_secret'
 
 
 @app.route('/')
@@ -13,11 +14,12 @@ def index():
 @app.route('/about')
 def about():
     data = []
-    with open("flask/data/company.json", "r") as json_data:
+    with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
 
-@app.route('/about/<member.name>')
+
+@app.route('/about/<member_name>')
 def about_member(member_name):
     member = {}
 
@@ -29,8 +31,13 @@ def about_member(member_name):
 
     return render_template("member.html", member=member)
 
-@app.route('/contact')
+
+@app.route('/contact', methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message".format(
+            request.form["name"]
+        ))
     return render_template("contact.html", page_title="Contact")
 
 
@@ -38,8 +45,7 @@ def contact():
 def careers():
     return render_template("careers.html", page_title="Careers")
 
-
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT', '3000')),
+            port=int(os.environ.get('PORT', '8080')),
             debug=True)
